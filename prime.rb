f4d054e5221832577d2fbdc2e14b1764e34f6d21
Matthias Rudin <matthias.rudin@live.com>
2018-11-23 05:15:26 +0000

    automatically backed up by learn

--- prime.rb
+++ prime.rb
@@ -1,6 +1,8 @@
 # Add  code here!
 def prime?(num)
-  return false if num <= 1
+  if number <= 1
+    return false
+  end
   Math.sqrt(num).to_i.downto(2).each {|i| return false if num % i == 0}
   true
 end
